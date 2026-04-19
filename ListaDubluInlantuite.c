9890664613492f4d521c3d7cfa16e94fa507ea3b
stamatescustefan-asestud <stamatescustefan24@stud.ase.ro>
2026-04-19 22:02:52 +0300

    ldi

---
+++ ListaDubluInlantuite.c
@@ -0,0 +1,112 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct StructuraMasina {
+	int id;
+	int nrUsi;
+	float pret;
+	char* model;
+	char* numeSofer;
+	unsigned char serie;
+};
+
+typedef struct StructuraMasina Masina;
+
+struct Nod {
+	Masina masina;
+	struct Nod* next;
+	struct Nod* prev;
+};
+
+typedef struct Nod Nod;
+
+struct ListaDubla {
+	Nod* first;
+	Nod* last;
+	int nrNoduri;
+};
+typedef ListaDubla ListaDubla;
+
+Masina citireMasinaDinFisier(FILE* file) {
+	char buffer[100];
+	char sep[3] = ",\n";
+	fgets(buffer, 100, file);
+	char* aux;
+	Masina m1;
+	aux = strtok(buffer, sep);
+	m1.id = atoi(aux);
+	m1.nrUsi = atoi(strtok(NULL, sep));
+	m1.pret = atof(strtok(NULL, sep));
+	aux = strtok(NULL, sep);
+	m1.model = malloc(strlen(aux) + 1);
+	strcpy_s(m1.model, strlen(aux) + 1, aux);
+
+	aux = strtok(NULL, sep);
+	m1.numeSofer = malloc(strlen(aux) + 1);
+	strcpy_s(m1.numeSofer, strlen(aux) + 1, aux);
+
+	m1.serie = *strtok(NULL, sep);
+	return m1;
+}
+
+void afisareMasina(Masina masina) {
+	printf("Id: %d\n", masina.id);
+	printf("Nr. usi : %d\n", masina.nrUsi);
+	printf("Pret: %.2f\n", masina.pret);
+	printf("Model: %s\n", masina.model);
+	printf("Nume sofer: %s\n", masina.numeSofer);
+	printf("Serie: %c\n\n", masina.serie);
+}
+
+void afisareListaMasiniDeLaInceput(ListaDubla lista) {
+	Nod* p = lista.first;
+	while (p) {
+		afisareMasina(p->masina);
+		p = p->next;
+	}
+}
+
+void afisareListaMasiniDeLaSfarsit(ListaDubla lista) {
+	Nod* p = lista.last;
+	while (p) {
+		afisareMasina(p->masina);
+		p = p->prev;
+	}
+}
+
+void adaugaMasinaInLista(ListaDubla* lista, Masina masinaNoua) {
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->masina = masinaNoua;
+	nou->next = NULL;
+	nou->prev = lista->last;
+	if (lista->last != NULL) {
+		lista->last->next = nou;
+	}
+	else {
+		lista->first = nou;
+	}
+	lista->last = nou;
+	lista->nrNoduri++;
+}
+
+void adaugaLaInceputInLista(ListaDubla* lista, Masina masinaNoua) {
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->masina = masinaNoua;
+	nou->next = lista->first;
+	nou->prev = NULL;
+	if (lista->first != NULL) {
+		lista->first->prev = nou;
+	}
+	else {
+		lista->last = nou;
+	}
+	lista->first = nou;
+	lista->nrNoduri++;
+}
+
+void* citireLDMasiniDinFisier(const char* numeFisier) {
+	FILE* f = fopen(numeFisier, "r");
+
+}
